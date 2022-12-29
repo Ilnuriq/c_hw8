80f0752b046340e3f5502360e4cd558bb5e22c11
Ilnur Garrapov <garrapovilnur@gmail.com>
2022-12-29 10:45:03 +0300

    62nd ex

--- Program.cs
+++ Program.cs
@@ -70,80 +70,40 @@ void CreateArray(int[,,] array3D)
 }
 */
 
-using System;
+int n = 4;
+int[,] sqareMatrix = new int[n, n];
 
-namespace task60
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
 {
-    class Program
+  sqareMatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(sqareMatrix);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        static void Main(string[] args)
-        {
-            int deep1 = InputInt("Введите размерность 1: ");
-            int deep2 = InputInt("Введите размерность 2: ");
-            int deep3 = InputInt("Введите размерность 3: ");
-            int countNums = 89;
-            
-            if (deep1 * deep2 * deep3 > countNums)
-            {
-               Console.Write("Массив слишком большой");
-                return;
-            }
-            
-            int[,,] resultNums = Create3DMassive(deep1, deep2, deep3);
-            
-            for (int i = 0; i < resultNums.GetLength(0); i++)
-            {
-                for (int j = 0; j < resultNums.GetLength(1); j++)
-                {
-                    for (int k = 0; k < resultNums.GetLength(2); k++)
-                    {
-                        Console.WriteLine($"[{i},{j},{k}] - {resultNums[i, j, k]}");
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine();
-            }
-            
-            
-            int[,,] Create3DMassive(int size1, int size2, int size3)
-            {
-                int[,,] array = new int[size1, size2, size3];
-                int[] values = new int[countNums];
-                int num
-                = 10;
-                for (int i = 0; i < values.Length; i++)
-                values[i] = num
-                ++;
-                
-                for (int i = 0; i < values.Length; i++)
-                {
-                    int randomInd = new Random().Next(0, values.Length);
-                    int temp = values[i];
-                    values[i] = values[randomInd];
-                    values[randomInd] = temp;
-                }
-                
-                int valueIndex = 0;
-                
-                for (int i = 0; i < array.GetLength(0); i++)
-                {
-                    for (int j = 0; j < array.GetLength(1); j++)
-                    {
-                        for (int k = 0; k < array.GetLength(2); k++)
-                        {
-                            array[i, j, k] = values[valueIndex++];
-                        }
-                    }
-                }
-                return array;
-            }
-            
-            
-            int InputInt(string output)
-            {
-                Console.Write(output);
-                return int.Parse(Console.ReadLine());
-            }
-        }
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
     }
+    Console.WriteLine();
+  }
 }
