662c3da1660c3d7d2b06e13baa5a5eb9a1c5773f
Ilnur Garrapov <garrapovilnur@gmail.com>
2022-12-30 17:00:38 +0300

    54th ex

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿/*
+//Задача 60.
 Console.WriteLine($"Введите размер массива X x Y x Z: ");
 int x = InputNumbers("Введите X: ");
 int y = InputNumbers("Введите Y: ");
@@ -68,8 +69,9 @@ void CreateArray(int[,,] array3D)
     }
   }
 }
-*/
 
+/*
+//Задача 62
 int n = 4;
 int[,] sqareMatrix = new int[n, n];
 
@@ -107,3 +109,60 @@ void WriteArray (int[,] array)
     Console.WriteLine();
   }
 }
+*/
+
+//Задача 54
+Console.WriteLine("введите количество строк");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine();
+Console.WriteLine("Массив до изменения");
+PrintArray(numbers);
+            
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+      {
+          for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+            {
+                if (numbers[i, z] < numbers[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
+                {
+                  int temp = 0;
+                  temp = numbers[i, z];
+                  numbers[i, z] = numbers[i, z + 1];
+                  numbers[i, z + 1] = temp;
+                }
+            }
+      }
+}
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями");
+PrintArray(numbers);
+            
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+          array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}    
